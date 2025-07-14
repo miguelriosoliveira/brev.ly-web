dd2774f0dc9ee9fcb4e283bfa2e16d7920d3bb29
Miguel Rios <miguelriosoliveira@gmail.com>
2025-07-14 18:04:23 +0200

    wip: empty page mobile

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,11 +1,51 @@
-import logoIconUrl from './assets/logo-icon.svg';
+import logoIconUrl from './assets/logo.svg';
+import { Download } from './icons/download';
+import { Link } from './icons/link';
 
 export function App() {
   return (
-    <>
+    <div className="flex h-dvh flex-col items-center gap-6 px-3 py-8">
       {/** biome-ignore lint/performance/noImgElement: Not a Next project */}
-      <img aria-label="Brev.ly logo" src={logoIconUrl} />
-      <h1>hello</h1>
-    </>
+      <img aria-label="Brev.ly logo" className="h-7" src={logoIconUrl} />
+
+      <div className="flex w-full flex-col gap-3">
+        <div className="rounded-lg bg-gray-100 p-6">
+          <h2 className="text-lg-bold">Novo link</h2>
+
+          <form>
+            <label className="text-xs-uppercase" htmlFor="original_link">
+              Link original
+            </label>
+            <input
+              name="original_link"
+              placeholder="www.exemplo.com.br"
+              type="url"
+            />
+
+            <label className="text-xs-uppercase" htmlFor="short_link">
+              Link encurtado
+            </label>
+            <input name="short_link" placeholder="brev.ly/" type="url" />
+
+            <button type="submit">Salvar link</button>
+          </form>
+        </div>
+
+        <div className="rounded-lg bg-gray-100 p-6">
+          <h2 className="text-lg-bold">Meus links</h2>
+          <button disabled type="button">
+            <Download />
+            Baixar CSV
+          </button>
+
+          <hr />
+
+          <Link />
+          <span className="text-xs-uppercase">
+            Ainda não existem links cadastrados
+          </span>
+        </div>
+      </div>
+    </div>
   );
 }
