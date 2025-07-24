97b507dc515499d924db42b9d6ee6912e5dc15ba
Miguel Rios <miguelriosoliveira@gmail.com>
2025-07-24 18:55:59 +0200

    chore: Triggers parent repo workflow

--- src/app.tsx
+++ src/app.tsx
@@ -24,7 +24,7 @@ export function App() {
     {
       originalLink: 'http://exemple.com',
       shortLink: 'exemplo',
-      accessCount: 3,
+      accessCount: 4,
     },
     {
       originalLink: 'http://exemple.com',
